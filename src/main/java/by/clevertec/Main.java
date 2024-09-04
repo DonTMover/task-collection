8aeb6b32a18d71798ac03a495b17e53c70bb3097
DonTMover <antantov624@gmail.com>
2024-09-04 22:00:52 +0300

    task14->task22 excluding task13

--- src/main/java/by/clevertec/Main.java
+++ src/main/java/by/clevertec/Main.java
@@ -11,9 +11,8 @@ import by.clevertec.util.Util;
 
 import java.time.LocalDate;
 import java.time.Period;
-import java.util.Comparator;
-import java.util.List;
-import java.util.Set;
+import java.util.*;
+import java.util.concurrent.Flow;
 import java.util.stream.Collectors;
 
 public class Main {
@@ -43,6 +42,9 @@ public class Main {
         task20();
         task21();
         task22();
+    }
+    private static int getAge(Person person){
+        return Period.between(person.getDateOfBirth(), LocalDate.now()).getYears();
     }
 
     public static void task1() {
@@ -175,15 +177,14 @@ public class Main {
 
     public static void task12() {
         List<Person> persons = Util.getPersons();
-        LocalDate currentDate = LocalDate.now();
 //        persons.stream() Продолжить ...
         List<Person> selectedCandidates = persons.stream()
                 .filter(person -> person.getGender().equalsIgnoreCase("Male") &&
-                        Period.between(person.getDateOfBirth(), currentDate).getYears() >= 18 &&
-                        Period.between(person.getDateOfBirth(), currentDate).getYears() <= 27)
+                        getAge(person) >= 18 &&
+                        getAge(person) <= 27)
                 .sorted(Comparator.comparingInt(Person::getRecruitmentGroup)
                         .reversed()
-                        .thenComparing(person -> Period.between(person.getDateOfBirth(), currentDate).getYears()))
+                        .thenComparing(person -> getAge(person)))
                 .limit(200)
                 .collect(Collectors.toList());
         selectedCandidates.stream().forEach(System.out::println);
@@ -192,56 +193,192 @@ public class Main {
     public static void task13() {
         List<House> houses = Util.getHouses();
 //        houses.stream() Продолжить ...
-//        houses.stream()
-//                .filter(house -> house.getBuildingType().equalsIgnoreCase("Hospital"))
+//        List<Person> people = houses.stream()
 //                .flatMap(house -> house.getPersonList().stream())
-//                .
+//                .collect(Collectors.toList());
+//
+//        people.stream()
+//                .sorted(Comparator.comparing(person -> getAge(person) >= 65)
+//        TODO: Temporarily Skiped
     }
+
 
 
     public static void task14() {
         List<Car> cars = Util.getCars();
 //        cars.stream() Продолжить ...
+        List<Car> turkmenistanCars = cars.stream()
+                .filter(car -> car.getCarMake().equalsIgnoreCase("Jaguar") ||
+                        car.getColor().equalsIgnoreCase("White"))
+                .collect(Collectors.toList());
+        List<Car> uzbekistanCars = cars.stream()
+                .filter(car -> car.getMass() < 1500 &&
+                        car.getCarMake().equalsIgnoreCase("BMW") ||
+                        car.getCarMake().equalsIgnoreCase("Lexus") ||
+                        car.getCarModel().equalsIgnoreCase("Chrysler") ||
+                        car.getCarMake().equalsIgnoreCase("Toyota"))
+                .collect(Collectors.toList());
+        List<Car> kazakhstanCars = cars.stream()
+                .filter(car -> car.getColor().equalsIgnoreCase("Black") &&
+                        car.getMass() > 4000 ||
+                        car.getCarMake().equalsIgnoreCase("GMC") ||
+                        car.getCarMake().equalsIgnoreCase("Dodge"))
+                .collect(Collectors.toList());
+        List<Car> kyrgyzstanCars = cars.stream()
+                .filter(car -> car.getReleaseYear() < 1982 ||
+                        car.getCarModel().equalsIgnoreCase("Civic") ||
+                        car.getCarModel().equalsIgnoreCase("Cherokee"))
+                .collect(Collectors.toList());
+        List<Car> russiaCars = cars.stream()
+                .filter(car -> !(car.getColor().equalsIgnoreCase("Yellow") ||
+                        car.getColor().equalsIgnoreCase("Red") ||
+                        car.getColor().equalsIgnoreCase("Green") ||
+                        car.getColor().equalsIgnoreCase("Blue")) ||
+                        car.getPrice() > 40000)
+                .collect(Collectors.toList());
+        List<Car> mongoliaCars = cars.stream()
+                .filter(car -> !turkmenistanCars.contains(car) &&
+                        !uzbekistanCars.contains(car) &&
+                        !kazakhstanCars.contains(car) &&
+                        !kyrgyzstanCars.contains(car) &&
+                        !russiaCars.contains(car))
+                .collect(Collectors.toList());
+        double CarsSum = CarsSum(turkmenistanCars) +
+                CarsSum(uzbekistanCars) +
+                CarsSum(kazakhstanCars) +
+                CarsSum(kyrgyzstanCars) +
+                CarsSum(russiaCars) +
+                CarsSum(mongoliaCars);
+        System.out.println("CarsSum = " + CarsSum);
+
+    }
+    private static double CarsSum(List<Car> cars){
+        return cars.stream()
+                .mapToDouble(Car::getPrice)
+                .sum();
     }
 
     public static void task15() {
         List<Flower> flowers = Util.getFlowers();
 //        flowers.stream() Продолжить ...
+        double FlowersSum = flowers.stream()
+                .sorted(Comparator.comparing(Flower::getOrigin).reversed()
+                        .thenComparing(Flower::getPrice).reversed()
+                        .thenComparing(Flower::getWaterConsumptionPerDay).reversed())
+
+                .filter(flower -> flower.getCommonName().startsWith("S") &&
+                        flower.getCommonName().endsWith("C"))
+                .filter(Flower::isShadePreferred)
+                .filter(flower -> flower.getFlowerVaseMaterial().contains("Aluminum") ||
+                        flower.getFlowerVaseMaterial().contains("Steel"))
+                .mapToDouble(flower -> flower.getPrice() + flower.getWaterConsumptionPerDay() * (5 * 365) * 0.8) //0.8 - цена куба воды
+                .sum();
+        System.out.println("FlowersSum = " + FlowersSum);
     }
 
     public static void task16() {
         List<Student> students = Util.getStudents();
 //        students.stream() Продолжить ...
+        students.stream()
+                .filter(student -> student.getAge() < 18)
+                .sorted(Comparator.comparing(Student::getSurname))
+                .forEach(student -> System.out.println("Surname=" +
+                        student.getSurname() +
+                        ",Age=" +
+                        student.getAge()));
     }
 
     public static void task17() {
         List<Student> students = Util.getStudents();
 //        students.stream() Продолжить ...
+        Set<String> studentGroups = students.stream()
+                .map(Student::getGroup)
+                .collect(Collectors.toSet());
+        System.out.println("studentGroups = " + studentGroups);
+
     }
 
     public static void task18() {
         List<Student> students = Util.getStudents();
-        List<Examination> examinations = Util.getExaminations();
 //        students.stream() Продолжить ...
+        LinkedHashMap<String, Double> averageAgeStudentsOfFaculty = students.stream()
+                .collect(Collectors.groupingBy(Student::getFaculty,
+                        Collectors.averagingInt(Student::getAge)))
+                .entrySet()
+                .stream()
+                .sorted(Map.Entry.comparingByValue(Comparator.reverseOrder()))
+                .collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue,
+                        (oldValue, newValue) -> oldValue, LinkedHashMap::new));
+        averageAgeStudentsOfFaculty.forEach((faculty, averageAge) -> System.out.println("faculty=" + faculty + ",averageAge=" + averageAge));
+
     }
 
     public static void task19() {
         List<Student> students = Util.getStudents();
+        List<Examination> examinations = Util.getExaminations();
+        Set<String> groups = students.stream()
+                .map(Student::getGroup)
+                .collect(Collectors.toSet());
+        try(Scanner scanner = new Scanner(System.in)) {
+            String targetGroup = scanner.next();
 //        students.stream() Продолжить ...
+            if (targetGroup != null) {
+                if(groups.contains(targetGroup)) {
+                    List<Student> StudentsExamBetter4 = students.stream()
+                            .filter(student -> student.getGroup().equals(targetGroup))
+                            .filter(student -> examinations.stream()
+                                    .anyMatch(examination -> examination.getStudentId() == student.getId() &&
+                                            examination.getExam3() > 4))
+                            .collect(Collectors.toList());
+                    System.out.println("collect = " + StudentsExamBetter4);
+                }else{
+                    throw new RuntimeException("targetGroup not in Groups Set");
+                }
+            } else {
+                throw new RuntimeException("targetGroup not found!");
+            }
+        }
+
     }
 
     public static void task20() {
         List<Student> students = Util.getStudents();
+        List<Examination> examinations = Util.getExaminations();
 //        students.stream() Продолжить ...
+        Optional<String> facultyWithHighestAverageExam1 = students.stream()
+                .collect(Collectors.groupingBy(Student::getFaculty,
+                        Collectors.averagingInt(student -> {
+                            return examinations.stream()
+                                    .filter(exam -> exam.getStudentId() == student.getId())
+                                    .mapToInt(Examination::getExam1)
+                                    .findFirst()
+                                    .orElse(0);
+                        })))
+                .entrySet()
+                .stream()
+                .max(Map.Entry.comparingByValue())
+                .map(Map.Entry::getKey);
+        facultyWithHighestAverageExam1.ifPresent(faculty -> System.out.println("faculty=" + faculty));
     }
 
     public static void task21() {
         List<Student> students = Util.getStudents();
 //        students.stream() Продолжить ...
+        Map<String, Long> studensCountByGroup = students.stream()
+                .collect(Collectors.groupingBy(Student::getGroup, Collectors.counting()));
+        System.out.println("studensCountByGroup = " + studensCountByGroup);
     }
 
     public static void task22() {
         List<Student> students = Util.getStudents();
 //        students.stream() Продолжить ...
+        Map<String, Integer> minAgeByFaculty = students.stream()
+                .collect(Collectors.groupingBy(Student::getFaculty,
+                        Collectors.minBy(Comparator.comparingInt(Student::getAge))))
+                .entrySet()
+                .stream()
+                .collect(Collectors.toMap(Map.Entry::getKey, entry -> entry.getValue().get().getAge()));
+        System.out.println("minAgeByFaculty = " + minAgeByFaculty);
+
     }
 }
